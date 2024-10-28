d81f8829b5118da07c811c987d2601a72174801c
unknown <kreede929@gmail.com>
2024-10-28 17:58:00 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+const box = document.querySelector('.box')
+
+async function getUsers() {
+	try {
+		const res = await fetch('https://fakestoreapi.com/products')
+		const users = await res.json()
+		users.forEach(user => {
+			const card = document.createElement('div'),
+				image = document.createElement('img'),
+				h2 = document.createElement('h2'),
+				h3 = document.createElement('h3'),
+				p = document.createElement('p'),
+				a = document.createElement('a')
+			card.classList.add('card')
+			image.innerHTML = user.image
+			image.setAttribute('src', user.image)
+			h2.innerHTML = `Title: ${user.title}`
+			p.innerHTML = user.description
+			h3.innerHTML = `Price: ${user.price}`
+			card.append(image, h2, p, h3)
+			box.appendChild(card)
+		})
+	} catch (error) {
+		console.log(error.message)
+	}
+}
+
+getUsers()
